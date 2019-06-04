4c0bd62ee6d68b7473103dae77bc65eaab134700
Yordan <pm.yordan@gmail.com>
2019-06-04 20:57:53 +0300

    test vs code version control.

--- stopShop/src/index.js
+++ stopShop/src/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = '6969';
 const handlers = require('./handlers')
-console.log('server start at: '+ port)
+
 http.createServer((res, req) => {
    
     for (let handler of handlers) {
